28c3ac42287cf05500de0c7785cba8804a97e2f6
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-06-21 11:31:51 +0800

    feat: try to add mixin support

--- java21/build.gradle.kts
+++ java21/build.gradle.kts
@@ -11,15 +11,37 @@ java {
     withSourcesJar()
 }
 
+val joptSimpleVersion = "5.0.4"
+val asmVersion = "5.2"
+val slf4jVersion = "1.8.0-beta2"
+val jbAnnotationsVersion = "24.1.0"
+
+val gradleWrapperVersion = "8.5"
+
+val lwtsVersion = "1.1.0-SNAPSHOT"
+val shurikenVersion = "0.0.1-SNAPSHOT"
+
+val mixinVersion = "0.8.7-SNAPSHOT"
+val leavesApiVersion = "1.20.6-R0.1-SNAPSHOT"
+
 tasks.withType<JavaCompile>().configureEach {
     options.release.set(21)
 }
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-releases/")
+    maven("https://repo.spongepowered.org/repository/maven-public/")
+    maven("https://repo.leavesmc.org/snapshots")
 }
 
 dependencies {
+    implementation("net.sf.jopt-simple:jopt-simple:$joptSimpleVersion")
+    implementation("org.ow2.asm:asm:$asmVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("org.jetbrains:annotations:$jbAnnotationsVersion")
+    implementation("org.spongepowered:mixin:$mixinVersion")
+    implementation("org.leavesmc.leaves:leaves-api:$leavesApiVersion")
     implementation("io.sigpipe:jbsdiff:1.0")
 }
 
--- java21/src/main/java/org/leavesmc/leavesclip/Leavesclip.java
+++ java21/src/main/java/org/leavesmc/leavesclip/Leavesclip.java
@@ -1,5 +1,16 @@
 package org.leavesmc.leavesclip;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import launchwrapper.ITweaker;
+import launchwrapper.LaunchClassLoader;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,20 +18,27 @@ import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public final class Leavesclip {
+    private static final Logger logger = LoggerFactory.getLogger("Leavesclip");
+    private static final String DEFAULT_TWEAK = "org.spongepowered.asm.launch.MixinTweaker";
+    public static LaunchClassLoader classLoader;
+    public static Map<String, Object> blackboard = new HashMap<>();
 
     public static void main(final String[] args) {
+        new Leavesclip(args);
+    }
+
+    private Leavesclip(final String[] args) {
         if (Path.of("").toAbsolutePath().toString().contains("!")) {
-            System.err.println("Leavesclip may not run in a directory containing '!'. Please rename the affected folder.");
+            logger.error("Leavesclip may not run in a directory containing '!'. Please rename the affected folder.");
             System.exit(1);
         }
 
@@ -31,27 +49,124 @@ public final class Leavesclip {
         final URL[] classpathUrls = setupClasspath();
 
         final ClassLoader parentClassLoader = Leavesclip.class.getClassLoader().getParent();
-        final URLClassLoader classLoader = new URLClassLoader(classpathUrls, parentClassLoader);
+        classLoader = new LaunchClassLoader(classpathUrls, parentClassLoader);
 
-        final String mainClassName = findMainClass();
-        System.out.println("Starting " + mainClassName);
+        final OptionParser parser = new OptionParser();
+        parser.allowsUnrecognizedOptions();
 
-        final Thread runThread = new Thread(() -> {
-            try {
-                final Class<?> mainClass = Class.forName(mainClassName, true, classLoader);
-                final MethodHandle mainHandle = MethodHandles.lookup()
-                        .findStatic(mainClass, "main", MethodType.methodType(void.class, String[].class))
-                        .asFixedArity();
-                mainHandle.invoke((Object) args);
-            } catch (final Throwable t) {
-                throw Util.sneakyThrow(t);
+        final OptionSpec<String> tweakClassOption = parser
+                .accepts("tweakClass", "Tweak class(es) to load")
+                .withRequiredArg()
+                .defaultsTo(DEFAULT_TWEAK);
+        final OptionSpec<String> nonOption = parser.nonOptions();
+
+        final OptionSet options = parser.parse(args);
+        final List<String> tweakClassNames = new ArrayList<>(options.valuesOf(tweakClassOption));
+
+        final List<String> argumentList = new ArrayList<>();
+        // This list of names will be interacted with through tweakers. They can append to this list
+        // any 'discovered' tweakers from their preferred mod loading mechanism
+        // By making this object discoverable and accessible it's possible to perform
+        // things like cascading of tweakers
+        blackboard.put("TweakClasses", tweakClassNames);
+
+        // This argument list will be constructed from all tweakers. It is visible here so
+        // all tweakers can figure out if a particular argument is present, and add it if not
+        blackboard.put("ArgumentList", argumentList);
+
+        // This is to prevent duplicates - in case a tweaker decides to add itself or something
+        final Set<String> visitedTweakerNames = new HashSet<>();
+        // The 'definitive' list of tweakers
+        final List<ITweaker> allTweakers = new ArrayList<>();
+        try {
+            final List<ITweaker> pendingTweakers = new ArrayList<>(tweakClassNames.size() + 1);
+            // The list of tweak instances - may be useful for interoperability
+            blackboard.put("Tweaks", pendingTweakers);
+            // The primary tweaker (the first one specified on the command line) will actually
+            // be responsible for providing the 'main' name and generally gets called first
+            ITweaker primaryTweaker = null;
+            // This loop will terminate, unless there is some sort of pathological tweaker
+            // that reinserts itself with a new identity every pass
+            // It is here to allow tweakers to "push" new tweak classes onto the 'stack' of
+            // tweakers to evaluate allowing for cascaded discovery and injection of tweakers
+            while (!tweakClassNames.isEmpty()) {
+                for (final Iterator<String> it = tweakClassNames.iterator(); it.hasNext(); ) {
+                    final String tweakName = it.next();
+                    // Safety check - don't reprocess something we've already visited
+                    if (visitedTweakerNames.contains(tweakName)) {
+                        logger.warn("Tweak class name {} has already been visited -- skipping", tweakName);
+                        // remove the tweaker from the stack otherwise it will create an infinite loop
+                        it.remove();
+                        continue;
+                    } else {
+                        visitedTweakerNames.add(tweakName);
+                    }
+                    logger.info("Loading tweak class name {}", tweakName);
+
+                    // Ensure we allow the tweak class to load with the parent classloader
+                    classLoader.getClassLoaderExclusions().add(tweakName.substring(0, tweakName.lastIndexOf('.')));
+                    final ITweaker tweaker = (ITweaker) Class.forName(tweakName, true, classLoader)
+                            .getConstructor().newInstance();
+                    pendingTweakers.add(tweaker);
+
+                    // Remove the tweaker from the list of tweaker names we've processed this pass
+                    it.remove();
+                    // If we haven't visited a tweaker yet, the first will become the 'primary' tweaker
+                    if (primaryTweaker == null) {
+                        logger.info("Using primary tweak class name {}", tweakName);
+                        primaryTweaker = tweaker;
+                    }
+                }
+
+                // Configure environment to avoid warn
+                configureMixin();
+
+                // Now, iterate all the tweakers we just instantiated
+                while (!pendingTweakers.isEmpty()) {
+                    final ITweaker tweaker = pendingTweakers.removeFirst();
+                    logger.info("Calling tweak class {}", tweaker.getClass().getName());
+                    tweaker.acceptOptions(options.valuesOf(nonOption));
+                    tweaker.injectIntoClassLoader(classLoader);
+                    allTweakers.add(tweaker);
+                }
+                // continue around the loop until there's no tweak classes
             }
-        }, "ServerMain");
-        runThread.setContextClassLoader(classLoader);
-        runThread.start();
+
+            // Once we're done, we then ask all the tweakers for their arguments and add them all to the
+            // master argument list
+            for (final ITweaker tweaker : allTweakers) {
+                argumentList.addAll(Arrays.asList(tweaker.getLaunchArguments()));
+            }
+
+            final String mainClassName = findMainClass();
+            logger.info("Starting {}", mainClassName);
+
+            final Thread runThread = new Thread(() -> {
+                try {
+                    argumentList.addAll(Arrays.asList(args));
+                    final Class<?> mainClass = Class.forName(mainClassName, true, classLoader);
+                    final MethodHandle mainHandle = MethodHandles.lookup()
+                            .findStatic(mainClass, "main", MethodType.methodType(void.class, String[].class))
+                            .asFixedArity();
+                    mainHandle.invoke((Object) argumentList.toArray(new String[0]));
+                } catch (final Throwable t) {
+                    throw Util.sneakyThrow(t);
+                }
+            }, "ServerMain");
+            runThread.setContextClassLoader(classLoader);
+            runThread.start();
+        } catch (Exception e) {
+            logger.error("Unable to launch", e);
+            System.exit(1);
+        }
     }
 
-    private static URL[] setupClasspath() {
+    private void configureMixin() {
+        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.SERVER);
+        Mixins.addConfiguration("mixins.akarin.core.json");
+    }
+
+    private static URL @NotNull [] setupClasspath() {
         final var repoDir = Path.of(System.getProperty("bundlerRepoDir", ""));
 
         final PatchEntry[] patches = findPatches();
